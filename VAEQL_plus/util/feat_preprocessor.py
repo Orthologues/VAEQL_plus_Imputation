94506d94f67def6ac71b456776e3e198066f79ee
jiz-mint <jwz.student.bmc.lu@gmail.com>
2026-03-01 11:52:18 +0100

    refactored the package naming

---
+++ VAEQL_plus/util/feat_preprocessor.py
@@ -0,0 +1,10 @@
+#########################################################
+# Author： Jiawei Zhao
+# Email1: jiz@sdu.dk
+# Email2: jwz.student.bmc.lu@gmail.com
+# Date: 2026-02-19
+# Desicrption: Feature preprocessor utilities for the VAE-Q learning imputation baseline based on the provided features in "VAEQL_plus.conf.FeaturesTypeDict".
+#########################################################
+
+from ..conf import FeaturesTypeDict
+
